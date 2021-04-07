4200581a220c0153b7bea677bef9a553bf53e98f
imjaeyeong3 <imjaeyeong3@gmail.com>
2021-04-07 19:07:45 +0900

    자바스크립트 타입

---
+++ js-type/type.js
@@ -0,0 +1,12 @@
+// let은 다른 값을 추후에 대입하거나 값을 변경할 수 있어 자유롭다
+// const는 선언 후에 다른 값을 대입하거나 값을 변경할 수 없다
+// var
+
+let letNum = 0;
+letNum = "10";
+
+const constNum = 0;
+
+var varNum = 0;
+varNum = 10;
+var varNum = 10;
