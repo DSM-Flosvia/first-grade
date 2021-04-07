29fa286f216bca1be78e354c665ee37141ab9a0a
hwangjinseong <imhjs2023@gmail.com>
2021-04-07 19:05:24 +0900

    자바스크립트

---
+++ js-type/type.js
@@ -0,0 +1,94 @@
+// let letNum=0;
+// letNum = "10";
+
+// const constNum = '0';
+
+// //let
+// //선언 후에 값이 변경될 수 있다.
+
+// //const
+// //선언 후에 값이 변경될 수 없다.
+
+// var varNum=0;
+// varNum = 10;
+
+// var varNum = 10;
+// //var
+// //선언 후에 값 변경 가능
+// //재 선언이 가능
+// //거의 사용되지 않음
+
+// //원시 타입 
+// //number 숫자(정수, 실수)
+// //string 문자열
+// //null 
+// //undefined 값이 안 넣어 졌을때 기본 값
+// //symbol
+// //boolean true or flase
+
+// //참조 타입
+// //object(객체)
+// // const obj={
+// //     key: "value",
+// //     key1: "value",
+// //     key2: "value",
+// // };
+// //array
+// const arr = ["문자", 0, 0];
+
+// const num = 0;
+// const num2 = num;
+
+// console.log(num);
+// console.log(num2);
+
+// const obj = {
+//     age: 19,
+// };
+// const obj2 = obj;
+
+// const obj2 = 17;
+
+// console.log(obj);
+// console.log(obj2);
+
+// const obj={
+//     key:"value"
+//     arr: [1, 2, 3, 4],
+//     obj: {
+//         key: "value",
+//     },
+// }
+//객체 접근 방법
+//1. obj.key
+//2. obj["key"]
+
+const arr = ["one", "two", "three", "four"];
+
+// for(let a of arr) {
+//     console.log(a);
+// }
+
+// for(let o in obj) {
+//     console.log(o,obj[o]);
+// }
+
+//배열 내장 함수
+
+//map
+//배열 순회
+// arr.map((e,i) => {
+//     console.log(i,e);
+// });
+
+// const mqpArray = arr.map((e,i) => {
+//     return e + "num";
+// })
+// console.log(mapArray);
+
+function func(a, b) {
+    return a + b;
+}
+
+const result = func(3, 2);
+console.log(result);
